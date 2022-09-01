7a67e9e1ed43c1ccfd1083099a1d09b49fadfaf6
Fury-Panda <furypanda09@gmail.com>
2022-09-01 06:04:15 -0400

    Initial upload.

---
+++ CyberDrill/Inter_University_Cyber_Drill_2022/Web/Question-2/packet_2013_prd_get_details.php
@@ -0,0 +1,7 @@
+<div class='pro_fulldesc_img_left'><img src='uploads/legion-5-0003-500x500.jpg' border='0' height='250px' width='280px' /></div><div class='pro_fulldesc_text'><pre>Model: Legion 5
+Ryzen 7 5800H Processor (3.2GHz Up to 4.4GHz, 8 Cores, 16 Threads)
+16GB 3200MHz RAM +1TB SSD
+GeForce RTX 3060 6GB Graphics
+15.6" FHD 165Hz (1920 x 1080) Display <br/><br/><a id='hide_details'><button type='button'>Close</button></a></pre></div><div class='pro_fulldesc_img_left'><img src='uploads/7' border='0' height='250px' width='280px' /></div><div class='pro_fulldesc_text'><pre>admin
+f2f7d2a4e7c1d1fcc4ef7e7968586c99aade8b5b1,demouser
+75c5b294454120dc49bdb9c40d3035da13ba48382 <br/><br/><a id='hide_details'><button type='button'>Close</button></a></pre></div>
---
+++ Pico_CTF/General/flag_shop/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	/* code */
+	long long int n, res;
+	scanf("%d",&n);
+	res=n/900;
+	printf("%d\n",res );
+	return 0;
+}
